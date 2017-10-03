021ff81afab8be30e552dc35b492c10f07d5ee39
SU <rumex13@naver.com>
2017-10-03 19:22:40 +0900

    orientdb
    write read

--- server_side_javascript/database_orientdb.js
+++ server_side_javascript/database_orientdb.js
@@ -1,11 +1,53 @@
 var OrientDB = require('orientjs');
 
 var server = OrientDB({
-    host: 'localhost',
-    port: 2480,
-    username: 'root',
-    password: '882530'
+   host:       'localhost',
+   port:       2424,
+   username:   'root',
+   password:   '882530'
 });
 
 var db = server.use('o2');
-// console.log('Using database: '+db.name);
+// db.record.get('#18:0').then(function(record) {
+//     console.log('Loaded record: ', record.title);
+// });
+
+// SELECT
+// var sql = 'SELECT FROM topic';
+// db.query(sql).then(function(results) {
+//     console.log(results);
+// });
+
+var sql = 'SELECT FROM topic WHERE @rid=:rid';
+var param = {
+    params : {
+        rid: '#18:0'
+    }
+};
+db.query(sql, param).then(function(results){
+    console.log(results);
+});
+
+// INSERT
+// var sql = 'INSERT INTO topic(title, description) VALUES (:title, :desc)';
+// db.query(sql, {
+//     params : {
+//         title: 'Express',
+//         desc: 'Express is framework for web'
+//     }
+// }).then(function(results) {
+//     console.log(results);
+// });
+
+//UPDATE
+// var sql = 'UPDATE topic SET title=:title WHERE @rid=:rid';
+// db.query(sql, {params:{title:'Expressjs', rid:'#21:0'}})
+// .then(function(results) {
+//     console.log(results);
+// });
+
+//DELETE
+var sql = 'DELETE VERTEX FROM topic WHERE @rid=:rid';
+db.query(sql, {params:{rid:'#21:0'}}).then(function(results) {
+    console.log(results);
+});
